3a85b14b93a702b86fe5f54734af21728f89dba3
DELL <alishcc1000@gmail.com>
2026-04-03 16:11:13 +0200

    update miniapp

--- src/App.jsx
+++ src/App.jsx
@@ -102,6 +102,10 @@ export default function App() {
 
 // ============================================================================= //
 function handleCreate() {
+
+  console.log("CLICKED");
+  alert("CLICKED");
+  
   const data = {
     user_id: 1,
 
