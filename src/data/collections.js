7a33fa759b322e5ce38d2fe3e05bdba5cfaff870
DELL <alishcc1000@gmail.com>
2026-04-03 18:12:13 +0200

    update miniapp

--- src/data/collections.js
+++ src/data/collections.js
@@ -59,4 +59,616 @@ export const collections = [
     image: "/assets/collections_preview/5296631769112525274.png",
     max_backdrop_id: 59
   },
+  {
+    id: "5825480571261813595",
+    name: "Evil Eye",
+    image: "/assets/collections_preview/5472306823555985042.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5825801628657124140",
+    name: "Hex Pot",
+    image: "/assets/collections_preview/5289764034801841061.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5825895989088617224",
+    name: "Hypno Lollipop",
+    image: "/assets/collections_preview/5230974475209554508.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5830340739074097859",
+    name: "Victory Medal",
+    image: "/assets/collections_preview/5253505577291641700.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5832644211639321671",
+    name: "Pool Float",
+    image: "/assets/collections_preview/5255734132742324362.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5836780359634649414",
+    name: "Voodoo Doll",
+    image: "/assets/collections_preview/5249244862359812334.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5837059369300132790",
+    name: "Scared Cat",
+    image: "/assets/collections_preview/5256041592271157291.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5837063436634161765",
+    name: "Flying Broom",
+    image: "/assets/collections_preview/5278491193053822590.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5839038009193792264",
+    name: "Skull Flower",
+    image: "/assets/collections_preview/5240364622338340370.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5841336413697606412",
+    name: "Crystal Ball",
+    image: "/assets/collections_preview/5271810272640643747.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5841391256135008713",
+    name: "Trapped Heart",
+    image: "/assets/collections_preview/5330077212022422738.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5841632504448025405",
+    name: "Mad Pumpkin",
+    image: "/assets/collections_preview/5472078009173292677.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5841689550203650524",
+    name: "Sharp Tongue",
+    image: "/assets/collections_preview/5220049530107475342.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5843762284240831056",
+    name: "Ion Gem",
+    image: "/assets/collections_preview/5201914481671682382.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5845776576658015084",
+    name: "Kissed Frog",
+    image: "/assets/collections_preview/5269679114163338720.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5846192273657692751",
+    name: "Electric Skull",
+    image: "/assets/collections_preview/5220037761897085778.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5846226946928673709",
+    name: "Magic Potion",
+    image: "/assets/collections_preview/5258208871423425369.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5856973938650776169",
+    name: "Record Player",
+    image: "/assets/collections_preview/5273738292049762668.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5857140566201991735",
+    name: "Vintage Cigar",
+    image: "/assets/collections_preview/5271480608130883261.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5859442703032386168",
+    name: "Gem Signet",
+    image: "/assets/collections_preview/5352734865315881645.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5868220813026526561",
+    name: "Toy Bear",
+    image: "/assets/collections_preview/5293983582472135373.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5868348541058942091",
+    name: "Love Potion",
+    image: "/assets/collections_preview/5294256463219291541.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5868455043362980631",
+    name: "Heart Locket",
+    image: "/assets/collections_preview/5294430336380330453.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5868503709637411929",
+    name: "Diamond Ring",
+    image: "/assets/collections_preview/5294459769791210746.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5868561433997870501",
+    name: "Cupid Charm",
+    image: "/assets/collections_preview/5294476812221439592.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5868595669182186720",
+    name: "Valentine Box",
+    image: "/assets/collections_preview/5292099655197289800.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5868659926187901653",
+    name: "Loot Bag",
+    image: "/assets/collections_preview/5294539325470434034.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5870661333703197240",
+    name: "Bonded Ring",
+    image: "/assets/collections_preview/5463153698492409496.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5870720080265871962",
+    name: "Nail Bracelet",
+    image: "/assets/collections_preview/5463206848712697897.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5870784783948186838",
+    name: "Restless Jar",
+    image: "/assets/collections_preview/5291877442179329766.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5870862540036113469",
+    name: "Joyful Bundle",
+    image: "/assets/collections_preview/5291741351845587729.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5870947077877400011",
+    name: "Sky Stilettos",
+    image: "/assets/collections_preview/5294208393945311238.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5870972044522291836",
+    name: "Input Key",
+    image: "/assets/collections_preview/5465502401358226185.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5871002671934079382",
+    name: "Lush Bouquet",
+    image: "/assets/collections_preview/5292138056499884476.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5879737836550226478",
+    name: "Mini Oscar",
+    image: "/assets/collections_preview/5283006569481525574.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5882125812596999035",
+    name: "Eternal Rose",
+    image: "/assets/collections_preview/5276453866727038041.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5882252952218894938",
+    name: "Berry Box",
+    image: "/assets/collections_preview/5276397508166181441.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5886387158889005864",
+    name: "Timeless Book",
+    image: "/assets/collections_preview/5310162878894999252.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5886756255493523118",
+    name: "Mood Pack",
+    image: "/assets/collections_preview/5307949733786976205.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5895328365971244193",
+    name: "Heroic Helmet",
+    image: "/assets/collections_preview/5318884376925204522.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5895518353849582541",
+    name: "Mighty Arm",
+    image: "/assets/collections_preview/5319126771994490119.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5895544372761461960",
+    name: "Bow Tie",
+    image: "/assets/collections_preview/5318925922143857841.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5895603153683874485",
+    name: "Fresh Socks",
+    image: "/assets/collections_preview/5321016003553945898.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5897581235231785485",
+    name: "Light Sword",
+    image: "/assets/collections_preview/5318865556378511858.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5897593557492957738",
+    name: "Top Hat",
+    image: "/assets/collections_preview/5321086861924395647.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5898012527257715797",
+    name: "Vice Cream",
+    image: "/assets/collections_preview/5321019366513344041.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5900177027566142759",
+    name: "Ice Cream",
+    image: "/assets/collections_preview/5323452998062469577.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5902339509239940491",
+    name: "Bling Binky",
+    image: "/assets/collections_preview/5323657958196804832.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5913442287462908725",
+    name: "Spiced Wine",
+    image: "/assets/collections_preview/5330435502489231897.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5913517067138499193",
+    name: "Perfume Bottle",
+    image: "/assets/collections_preview/5272007523308689132.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5915502858152706668",
+    name: "Jelly Bunny",
+    image: "/assets/collections_preview/5292285936518849387.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5915521180483191380",
+    name: "Durov’s Cap",
+    image: "/assets/collections_preview/5330191715850541636.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5915550639663874519",
+    name: "Love Candle",
+    image: "/assets/collections_preview/5285388657128136494.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5915733223018594841",
+    name: "Hanging Star",
+    image: "/assets/collections_preview/5287433345028872301.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5933531623327795414",
+    name: "Genie Lamp",
+    image: "/assets/collections_preview/5312031339992603513.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5933543975653737112",
+    name: "Whip Cupcake",
+    image: "/assets/collections_preview/5359288375524222371.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5933590374185435592",
+    name: "Jester Hat",
+    image: "/assets/collections_preview/5301170205219642529.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5933629604416717361",
+    name: "Astral Shard",
+    image: "/assets/collections_preview/5325685779760962109.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5933671725160989227",
+    name: "Precious Peach",
+    image: "/assets/collections_preview/5292092955048302153.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5933737850477478635",
+    name: "Pretty Posy",
+    image: "/assets/collections_preview/5357304963921898169.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5933793770951673155",
+    name: "Neko Helmet",
+    image: "/assets/collections_preview/5359694133969576053.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5933937398953018107",
+    name: "Ionic Dryer",
+    image: "/assets/collections_preview/5357444460164707026.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5935877878062253519",
+    name: "Mousse Cake",
+    image: "/assets/collections_preview/5357565320544415913.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5935936766358847989",
+    name: "Bunny Muffin",
+    image: "/assets/collections_preview/5289749346013697883.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5936013938331222567",
+    name: "Plush Pepe",
+    image: "/assets/collections_preview/5334887566804016544.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5936017773737018241",
+    name: "Star Notepad",
+    image: "/assets/collections_preview/5361753922975348287.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5936043693864651359",
+    name: "Swiss Watch",
+    image: "/assets/collections_preview/5291963006517795791.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5936085638515261992",
+    name: "Signet Ring",
+    image: "/assets/collections_preview/5348330402123637435.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5960747083030856414",
+    name: "Clover Pin",
+    image: "/assets/collections_preview/5384279583333839726.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5963238670868677492",
+    name: "Money Pot",
+    image: "/assets/collections_preview/5384540360863150750.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5980789805615678057",
+    name: "Snow Mittens",
+    image: "/assets/collections_preview/5370570421387157397.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5981026247860290310",
+    name: "Sleigh Bell",
+    image: "/assets/collections_preview/5377319674730010551.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5981132629905245483",
+    name: "Snow Globe",
+    image: "/assets/collections_preview/5359452507699436094.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5983259145522906006",
+    name: "Winter Wreath",
+    image: "/assets/collections_preview/5384447215907396732.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5983471780763796287",
+    name: "Santa Hat",
+    image: "/assets/collections_preview/5384530624172288606.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5983484377902875708",
+    name: "Ginger Cookie",
+    image: "/assets/collections_preview/5384532380813910923.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "5998981470310368313",
+    name: "Moon Pendant",
+    image: "/assets/collections_preview/5422868769271738074.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5999116401002939514",
+    name: "Rare Bird",
+    image: "/assets/collections_preview/5422517917688296142.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "5999277561060787166",
+    name: "Chill Flame",
+    image: "/assets/collections_preview/5424672908939130073.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6001473264306619020",
+    name: "Jingle Bells",
+    image: "/assets/collections_preview/5384510480775669175.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "6001538689543439169",
+    name: "Cookie Heart",
+    image: "/assets/collections_preview/5362088994848923447.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "6003373314888696650",
+    name: "Candy Cane",
+    image: "/assets/collections_preview/5425020328843699498.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6003456431095808759",
+    name: "Faith Amulet",
+    image: "/assets/collections_preview/5427103310672853316.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6003643167683903930",
+    name: "Party Sparkler",
+    image: "/assets/collections_preview/5359683967781986706.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "6003735372041814769",
+    name: "Holiday Drink",
+    image: "/assets/collections_preview/5424735731425762513.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6003767644426076664",
+    name: "Xmas Stocking",
+    image: "/assets/collections_preview/5422391899052860201.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6005564615793050414",
+    name: "Instant Ramen",
+    image: "/assets/collections_preview/5429354096874253649.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6005659564635063386",
+    name: "Jack-in-the-Box",
+    image: "/assets/collections_preview/5429360500670490737.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6005797617768858105",
+    name: "Artisan Brick",
+    image: "/assets/collections_preview/5429371521556575113.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6005880141270483700",
+    name: "Jolly Chimp",
+    image: "/assets/collections_preview/5429359104806118631.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6006064678835323371",
+    name: "Happy Brownie",
+    image: "/assets/collections_preview/5429377594640333870.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6012435906336654262",
+    name: "Snoop Cigar",
+    image: "/assets/collections_preview/5436120792079302539.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6012607142387778152",
+    name: "Swag Bag",
+    image: "/assets/collections_preview/5438645653028762597.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6014591077976114307",
+    name: "Snoop Dogg",
+    image: "/assets/collections_preview/5438222109123834742.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6014675319464657779",
+    name: "Low Rider",
+    image: "/assets/collections_preview/5438223054016638005.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6014697240977737490",
+    name: "Westside Sign",
+    image: "/assets/collections_preview/5438548621127615575.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6023679164349940429",
+    name: "Snake Box",
+    image: "/assets/collections_preview/5447306828243301016.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6023752243218481939",
+    name: "Tama Gadget",
+    image: "/assets/collections_preview/5447111643454531858.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6023917088358269866",
+    name: "Pet Snake",
+    image: "/assets/collections_preview/5447469461474929443.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6028283532500009446",
+    name: "Big Year",
+    image: "/assets/collections_preview/5447490764512716380.png",
+    max_backdrop_id: 79
+  },
+  {
+    id: "6028426950047957932",
+    name: "Lunar Snake",
+    image: "/assets/collections_preview/5449588035698057178.png",
+    max_backdrop_id: 59
+  },
+  {
+    id: "6042113507581755979",
+    name: "Stellar Rocket",
+    image: "/assets/collections_preview/5465339480363795746.png",
+    max_backdrop_id: 79
+  },
 ];
